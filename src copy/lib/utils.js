a157d7895e126cd579062125c7ad8cbf85cf6acd
Guillaume Galuz <guillaumegaluz@gmail.com>
2021-06-10 15:21:20 -0400

    delete src copy folder

--- src copy/lib/utils.js
+++
@@ -1,9 +0,0 @@
-export const getNodeURL = () => {
-  return process.env.REACT_APP_USE_DATAHUB === "true"
-    ? `${process.env.REACT_APP_DATAHUB_DEVNET_URL}${process.env.REACT_APP_API_KEY}`
-    : process.env.REACT_APP_DEVNET_URL;
-}
-
-export const getExplorerAddressURL = (address) => {
-  return `https://explorer.solana.com/address/${address}?cluster=devnet`;
-}
