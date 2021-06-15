cb92b2734bb3d08e596256eed9ee08c65a887fe7
Guillaume Galuz <guillaumegaluz@gmail.com>
2021-06-15 11:13:28 -0400

    update urls again

--- src/lib/steps-config.js
+++ src/lib/steps-config.js
@@ -2,36 +2,36 @@ export const STEPS = [
   {
     id: "connect",
     title: "Connect to the Solana devnet cluster",
-    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/1.-connect-to-solana-devnet-using-datahub"
+    url: "https://learn.figment.io/network-documentation/solana/solana-pathway/connect"
   },
   {
     id: "account",
     title: "Create an Account/Keypair",
-    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/2.-create-your-first-solana-account"
+    url: "https://learn.figment.io/network-documentation/solana/solana-pathway/keypair"
   },
   {
     id: "fund",
     title: "Fund the account with SOL",
-    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/3.-fund-the-account-with-sol"
+    url: "https://learn.figment.io/network-documentation/solana/solana-pathway/fund"
   },
   {
     id: "balance",
     title: "Check your account's balance",
-    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/4.-check-the-account-balance"
+    url: "https://learn.figment.io/network-documentation/solana/solana-pathway/balance"
   },
   {
     id: "transfer",
     title: "Transfer SOL tokens between accounts",
-    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/5.-transfer-sol-tokens"
+    url: "https://learn.figment.io/network-documentation/solana/solana-pathway/transfer"
   },
   {
     id: "deploy",
     title: "Deploy a Program",
-    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/6.-deploy-a-program"
+    url: "https://learn.figment.io/network-documentation/solana/solana-pathway/deploy"
   },
   {
     id: "call",
     title: "Call a Program",
-    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/call-a-program"
+    url: "https://learn.figment.io/network-documentation/solana/solana-pathway/call"
   },
 ];
