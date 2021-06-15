5bcb11313d0118ef617cc3b8f34154a392d76ee2
Guillaume Galuz <guillaumegaluz@gmail.com>
2021-06-15 11:09:25 -0400

    update steps links

--- src/lib/steps-config.js
+++ src/lib/steps-config.js
@@ -2,36 +2,36 @@ export const STEPS = [
   {
     id: "connect",
     title: "Connect to the Solana devnet cluster",
-    url: "https://learn.figment.io/network-documentation/solana/tutorials/intro-pathway-solana-basics/1.-connect-to-solana-devnet-using-datahub"
+    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/1.-connect-to-solana-devnet-using-datahub"
   },
   {
     id: "account",
     title: "Create an Account/Keypair",
-    url: "https://learn.figment.io/network-documentation/solana/tutorials/intro-pathway-solana-basics/2.-create-your-first-solana-account"
+    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/2.-create-your-first-solana-account"
   },
   {
     id: "fund",
     title: "Fund the account with SOL",
-    url: "https://learn.figment.io/network-documentation/solana/tutorials/intro-pathway-solana-basics/3.-fund-the-account-with-sol"
+    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/3.-fund-the-account-with-sol"
   },
   {
     id: "balance",
     title: "Check your account's balance",
-    url: "https://learn.figment.io/network-documentation/solana/tutorials/intro-pathway-solana-basics/4.-check-the-account-balance"
+    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/4.-check-the-account-balance"
   },
   {
     id: "transfer",
     title: "Transfer SOL tokens between accounts",
-    url: "https://learn.figment.io/network-documentation/solana/tutorials/intro-pathway-solana-basics/5.-transfer-sol-tokens"
+    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/5.-transfer-sol-tokens"
   },
   {
     id: "deploy",
     title: "Deploy a Program",
-    url: "https://app.gitbook.com/@figment-learn/s/pathway-sandbox/solana-101-pathway/transfer-sol-between-accounts"
+    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/6.-deploy-a-program"
   },
   {
     id: "call",
     title: "Call a Program",
-    url: "https://app.gitbook.com/@figment-learn/s/pathway-sandbox/solana-101-pathway/transfer-sol-between-accounts"
+    url: "https://learn.figment.io/network-documentation/solana/intro-pathway-solana-basics/call-a-program"
   },
 ];
