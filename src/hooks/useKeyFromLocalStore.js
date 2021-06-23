fdae98759a2c015a7522a8783b58482c1d37d011
zurgl <zurgly@gmail.com>
2021-06-23 17:12:54 +0200

    Create useKeyFromLocalStore.js
    add a hooks to store generated keypair from step 2.
    It alllow user to keep into local storage the keypair then it solve some bugs

---
+++ src/hooks/useKeyFromLocalStore.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Keypair } from "@solana/web3.js";
+    
+export default function useKeyFromLocalStore() {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = JSON.parse(window.localStorage.getItem("solana_secret_key"));
+      if (!item) return null
+      
+      const secret = Uint8Array.from(item)
+      return Keypair.fromSecretKey(secret);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  });
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(Array.from(valueToStore._keypair.secretKey)));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+}
